8b7c7b7612de9be37b579aa5ca0bfa898a948049
Jason Kurniawan <mjkurnia@uwaterloo.ca>
2017-03-08 20:00:44 -0500

    add postback

--- index.js
+++ index.js
@@ -96,9 +96,30 @@ function receivedMessage(event) {
       default:
         sendTextMessage(senderID, messageText);
     }
-  } else if (messageAttachments) {
+  } 
+  else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
+  else if (event.postback) {
+    receivedPostback(event);
+  }
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
 }
 
 function sendGenericMessage(recipientId) {
@@ -182,28 +203,4 @@ function callSendAPI(messageData) {
     }
   });  
 }
-
-// function to echo back messages - added by Stefan
-/*function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/...',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-*/
-
 
