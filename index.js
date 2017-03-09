82531dfe912e9c9a55d3add011a265688871eeb4
Jason Kurniawan <mjkurnia@uwaterloo.ca>
2017-03-08 19:28:26 -0500

    add received message and send message functions

--- index.js
+++ index.js
@@ -100,9 +100,41 @@ function receivedMessage(event) {
   }
 }
 
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        //sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
 
 // function to echo back messages - added by Stefan
-function sendTextMessage(sender, text) {
+/*function sendTextMessage(sender, text) {
     messageData = {
         text:text
     }
@@ -122,5 +154,6 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+*/
 
 
